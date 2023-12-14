98f5752421ed605c5f3863d50955c2c68d5b4ff7
Gorachevsky <anton.gorachevskiy@gmail.com>
2023-12-14 03:06:19 +0100

    Added pagination logic for PDF

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,11 +1,14 @@
 "use client"
 
-import { Loader2 } from "lucide-react";
+import { ChevronDown, ChevronUp, Loader2 } from "lucide-react";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
 import { useToast } from "./ui/use-toast";
 import { useResizeDetector } from "react-resize-detector";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { useState } from "react";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
@@ -17,13 +20,47 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
 
   const { toast } = useToast();
 
+  const [numPages, setNumPages] = useState<number>();
+  const [currPage, setCurrPage] = useState<number>(1);
+
   const { width, ref } = useResizeDetector();
 
   return (
     <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
       <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
         <div className="flex items-center gap-1.5">
-          top bar
+          <Button 
+            disabled={currPage <= 1}
+            onClick={() => {
+              setCurrPage((prev) => prev - 1 > 1 ? prev - 1 : 1)
+            }}
+            variant="ghost" 
+            aria-label="previous page"
+          >
+            <ChevronDown className="h-4 w-4" />
+          </Button>
+
+          <div className="flex items-center gap-1.5">
+            <Input className="w-12 h-8" />
+            <p className="text-zinc-700 text-sm space-x-1">
+              <span>/</span>
+              <span>{numPages ?? "x"}</span>
+            </p>
+          </div>
+
+          <Button
+            disabled={
+              numPages === undefined ||
+              currPage === numPages
+            }
+            onClick={() => {
+              setCurrPage((prev) => prev + 1 > numPages! ? numPages! : prev + 1)
+            }}
+            variant="ghost"
+            aria-label="previous page"
+          >
+            <ChevronUp className="h-4 w-4" />
+          </Button>
         </div>
       </div>
 
@@ -35,17 +72,23 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
                 <Loader2 className="my-24 h-6 w-6 animate-spin" />
               </div>
             }
-            onError={() => {
+            onLoadError={() => {
               toast({
                 title: "Error loading PDF",
                 description: "Please try again later",
                 variant: "destructive",
               });
             }}
-            file={url} 
+            onLoadSuccess={({ numPages }) => {
+              setNumPages(numPages);
+            }}
+            file={url}
             className="max-h-full"
           >
-            <Page width={width ? width : 1} pageNumber={1} />
+            <Page 
+              width={width ? width : 1} 
+              pageNumber={currPage}
+            />
           </Document>
         </div>
       </div>
