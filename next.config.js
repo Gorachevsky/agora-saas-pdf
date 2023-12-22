c4f5e4be56161e297d2bb5726ce110e38c26a557
Gorachevsky <anton.gorachevskiy@gmail.com>
2023-12-23 00:15:05 +0100

    Updated next config

--- next.config.js
+++ next.config.js
@@ -1,26 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirect() {
+  async redirects() {
     return [
       {
-        source: "/sign-in",
-        destination: "api/auth/login",
-        pemanent: true,
+        source: '/sign-in',
+        destination: '/api/auth/login',
+        permanent: true,
       },
       {
-        source: "/sign-in",
-        destination: "/api/auth/register",
+        source: '/sign-up',
+        destination: '/api/auth/register',
         permanent: true,
       },
     ]
   },
+
   webpack: (
     config,
-    { buildId, dev, isServer, defaulLoaders, webpack }
+    { buildId, dev, isServer, defaultLoaders, webpack }
   ) => {
-    config.resolve.alias.canvas = false;
-    config.resolve.alias.encoding = false;
-    return config;
+    config.resolve.alias.canvas = false
+    config.resolve.alias.encoding = false
+    return config
   },
 }
 
