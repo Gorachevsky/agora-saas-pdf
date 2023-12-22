5a03311223b7fc54b0de90f01641c050785dcd45
Gorachevsky <anton.gorachevskiy@gmail.com>
2023-12-22 23:24:47 +0100

    Added MobileNav to Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,7 @@ import { buttonVariants } from "./ui/button";
 import { LoginLink, RegisterLink, getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { ArrowRight } from "lucide-react";
 import UserAccountNav from "./UserAccountNav";
+import MobileNav from "./MobileNav";
 
 const Navbar = async () => {
 
@@ -19,6 +20,7 @@ const Navbar = async () => {
           </Link>
 
           {/* todo: add mobile navbar */}
+          <MobileNav />
 
           <div className="hidden items-center space-x-4 sm:flex">
             {!user ? (
