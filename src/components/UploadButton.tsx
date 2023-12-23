55c0c2689bb5949ff831bdb4bda5098fce617fee
Gorachevsky <anton.gorachevskiy@gmail.com>
2023-12-23 14:30:12 +0100

    Updated ux to show diferent file size options depending of the current plan

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -96,7 +96,7 @@ const UploadDropzone = ({ isSubscribed }: { isSubscribed: boolean }) => {
                   <span className="font-semibold">Click to upload</span>{" "}
                   or dag and drop
                 </p>
-                <p className="text-xs text-zinc-500">PDF (up to 4MB)</p>
+                <p className="text-xs text-zinc-500">PDF (up to {isSubscribed ? "16" : "4"}MB)</p>
               </div>
 
               {acceptedFiles && acceptedFiles[0] ? (
